267ad9cc9bc48ed31dbddbe047898d56db22b5c8
Your name <exaple@exaple.ru>
2018-07-08 15:00:32 +0700

    Modified index.html & App.js & index.js

--- src/App.js
+++ src/App.js
@@ -2,20 +2,46 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Grid from 'react-bootstrap/lib/Grid'
+import Row from 'react-bootstrap/lib/Row'
+import Col from 'react-bootstrap/lib/Col'
+
+import styled from 'styled-components';
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h1 className="App-title">Welcome to React</h1>
+//         </header>
+//         <p className="App-intro">
+//           To get started, edit <code>src/App.js</code> and save to reload.
+//         </p>
+//       </div>
+//     );
+//   }
+// }
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="wrapper">
+        <SayFullName name="Ivan" surname="Ivanov" link="http://vk.com" />
+        <SayFullName name="Alena" surname="Petrova" link="http://fb.com" />
+        <SayFullName name="Irina" surname="Smirnova" link="http://ok.ru" />
       </div>
     );
   }
+}
+
+function SayFullName(props) {
+  return (
+     <div>
+      <h1> Moe имя {props.name}, фамилия - {props.surname}</h1>
+      <a href={props.link}> Ссылка на мой профиль   </a>
+    </div>
+  )   
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-new Function() {
+/*new Function() {
 	return (
 			<h1> Hello World </h1>
 		)
-}
+}*/
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
