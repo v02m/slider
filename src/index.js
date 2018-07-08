38f1da4703e6e71b130be1bec86aa9a79d8dc537
Your name <exaple@exaple.ru>
2018-07-08 14:23:59 +0700

    Add fackinf img in our project

--- src/index.js
+++ src/index.js
@@ -4,5 +4,11 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+new Function() {
+	return (
+			<h1> Hello World </h1>
+		)
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
